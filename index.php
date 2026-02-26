acffbed37097d937208594a6fcbf2811be346061
aranighosal1987-rgb <aranighosal1987@gmail.com>
2026-02-26 23:35:07 +0530

    another change

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello World from OpenShift Sanbox lab!";
-echo "My name is Arani and this is my code";
+echo "Hello World from OpenShift Sanbox lab! <br>";
+echo "My name is Arani and this is my code <br>";
 echo "this is cool";
 ?>
