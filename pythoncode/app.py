c10b1d2120d6867675faf419fcf41f3c70010b2b
aranighosal1987-rgb <aranighosal1987@gmail.com>
2026-03-03 18:16:15 +0530

    small changes on main

--- pythoncode/app.py
+++ pythoncode/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world from OpenShift!"
+    return "Hello world deployment from OpenShift!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
