338e5a1ef205ce4ab7d0bfc4bf739f77323f2659
aranighosal1987-rgb <aranighosal1987@gmail.com>
2026-03-17 23:49:15 +0530

    Hello World

--- pythoncode/app.py
+++ pythoncode/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return '<h1 style="color:pink; font-weight:bold;">Hello World!</h1>'
+    return '<h1 style="color:pink; font-weight:bold;">Hello World</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
